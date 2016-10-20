cf253550564b7e0d3d713b573350e2e3619c3ade
TomasVotruba <tomas.vot@gmail.com>
2016-10-20 16:20:20 +0200

    [ModularRouting] init

---
+++ tests/AppKernel.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Symplify\ModularRouting\Tests;
+
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Bundle\TwigBundle\TwigBundle;
+use Symfony\Cmf\Bundle\RoutingBundle\CmfRoutingBundle;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\HttpKernel\Kernel;
+use Symplify\ModularRouting\SymplifyModularRoutingBundle;
+
+final class AppKernel extends Kernel
+{
+    public function __construct()
+    {
+        parent::__construct(rand(1, 100), true);
+    }
+
+    public function registerBundles() : array
+    {
+        return [
+            new FrameworkBundle(),
+            new CmfRoutingBundle(),
+            new SymplifyModularRoutingBundle(),
+            new TwigBundle(),
+        ];
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(__DIR__.'/Resources/config/config.yml');
+    }
+}
